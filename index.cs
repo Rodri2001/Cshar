07c4ce852e62859d94d05fa0c2342536223891d3
Rodri2001 <rodrigodamianmartinez2001@gmail.com>
2023-05-19 20:42:50 -0300

    operadores condicionales

--- index.cs
+++ index.cs
@@ -39,3 +39,39 @@ mainCharacter = names[0]; // agarra el primero dela lista names[0] ahora maincha
 
 //operadores condicionales 
 bool win;
+int morescore;
+
+if (score == 500 || morescore > 100) // las condiciones funcionan igual que en javascript 
+{
+    win = true;
+}
+else
+{
+    win = false;
+}
+// otro operador condicional es el while 
+
+// mientras que score se mayor a 1 restale 1
+while (score > 0)
+{
+    score--;
+}
+
+// for es otro operador condicional 
+//  la primera es la inicializacion de la variable
+// la segunda sentencia es la condicion que se tiene que cumplir para que el bucle se repita
+// y la tercera es lo que queremos que haga en este caso queremos que le sume 1 a i
+for (int i = 0; i < 3; i++)
+{
+    score++;
+}
+
+string[] names = {"Mario","Luigi","Bowser"};
+
+
+// lo que hace este codigo es agregarle un . a cada elemento del array
+
+for(int i = 0; i < 3; i++){
+ names[i] += ".";
+}
+
