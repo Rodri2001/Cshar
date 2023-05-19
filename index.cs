d49d3297f66deab1f8ee526dc02ffcb2eb5e8921
Rodri2001 <rodrigodamianmartinez2001@gmail.com>
2023-05-19 20:19:25 -0300

    El comienzo

---
+++ index.cs
@@ -0,0 +1,17 @@
+// Cosas basicas para poder programar un video juego
+
+
+//para declarar variables 
+//integer, float ,sting , boolean
+//int para declarar tipo let 
+
+init score;
+score = 100;
+
+// float sirve para numeros decimales 
+
+float score2 = 00.1;
+
+// bool es para declarar variables del tipo boolean
+bool okey = true || false;
+
